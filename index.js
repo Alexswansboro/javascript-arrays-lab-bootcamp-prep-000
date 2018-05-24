497df6b6e3912d755d7324f0d03092491e942850
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-24 01:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
-function destructivelyAppendKitten("Ralph") {
+function destructivelyAppendKitten ("Ralph") {
   kittens.push("Ralph")
   return(kittens)
 }
